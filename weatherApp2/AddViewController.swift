34d662403bce0d089476fdf20c1b7fbddc2cab6c
Zosia <zosia@Zosias-iMac.local>
2018-11-11 10:55:37 -0800

    Initial for task 3

--- weatherApp2/AddViewController.swift
+++ weatherApp2/AddViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import CoreLocation
 
-class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
     
     var cities: [[String:Any]] = [[:]]
     var selectedCity: City!
+    var locationManager: CLLocationManager!
+    var currentCoordinates: CLLocationCoordinate2D!
     
+    @IBOutlet weak var currentLocation: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var clear: UIButton!
     @IBOutlet weak var search: UIButton!
     @IBOutlet weak var typeField: UITextField!
+    @IBOutlet weak var serachForCities: UIButton!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cities.count
@@ -27,6 +32,10 @@ class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         
         //nadpisanie dodawania
         cell.textLabel!.text = cities[indexPath.row]["title"] as? String
+        let distance = cities[indexPath.row]["distance"] as? String
+        if (distance != nil){
+            cell.detailTextLabel?.text = "\(distance!) km"
+        }
         return cell
     }
     
@@ -34,40 +43,75 @@ class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        self.getCurrentLocation()
+    }
+    
+    func getCurrentLocation(){
+        print("getting current location..")
+        if (CLLocationManager.locationServicesEnabled()){
+            print("locationServicesEnabled")
+            self.locationManager = CLLocationManager()
+            self.locationManager.delegate = self
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.startUpdatingLocation()
+            
+            if let location = locationManager.location {
+                CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+                    
+                    guard let placemark = placemarks?.first else {
+                        let errorString = error?.localizedDescription ?? "Unexpected Error"
+                        print("[Uops!] location reverse geocoding went wrong: \(errorString)")
+                        return
+                    }
+                    
+                    self.currentLocation.text = "You are in \(placemark.locality!)"
+                }
+            self.currentCoordinates = locationManager.location?.coordinate
+                print("coords saved")
+            }
+        } else {
+            print ("location services not enabled :(")
+        }
+        
+    }
+    
+    @IBAction func searchCities(_ sender: Any) {
+        cities.removeAll()
+        let urlString = "https://www.metaweather.com/api/location/search/?lattlong=\(currentCoordinates.latitude),\(currentCoordinates.longitude)"
+            self.callApi(urlString)
     }
     
     @IBAction func searchForCities(_ sender: Any){
-        print("In here")
+        cities.removeAll()
         if let textToSearch = typeField.text?.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed){
-            print("aaa")
             let urlString = "https://www.metaweather.com/api/location/search/?query=\(textToSearch)"
-            print(urlString)
-            guard let requestUrl = URL(string:urlString) else { return }
-            let request = URLRequest(url:requestUrl)
-            let task = URLSession.shared.dataTask(with: request) {
-                (data, response, error) in
-                if error == nil,let usableData = data {
-                    print(usableData)
-                    do {
-                        print("parsing....")
-                        guard let json = try JSONSerialization.jsonObject(with: usableData, options: .mutableContainers) as? [[String: Any]] else { print("json")
-                            return }
-                        print ("parsed!")
-                        print(json)
-                        self.cities = json
-                        
-                        DispatchQueue.main.async {
-                            self.updateView()
-                        }
-                    } catch {
-                        print(error)
-                        return
+            self.callApi(urlString)
+            
+        }
+    }
+    
+    func callApi (_ urlString: String){
+        guard let requestUrl = URL(string:urlString) else { return }
+        let request = URLRequest(url:requestUrl)
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in
+            if error == nil,let usableData = data {
+                print(usableData)
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: usableData, options: .mutableContainers) as? [[String: Any]] else { print("json")
+                        return }
+                    self.cities = json
+                    
+                    DispatchQueue.main.async {
+                        self.updateView()
                     }
+                } catch {
+                    print(error)
+                    return
                 }
             }
-            task.resume()
         }
+        task.resume()
     }
     
     func updateView(){
@@ -95,7 +139,7 @@ class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataS
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
+     
      }
      */
     
